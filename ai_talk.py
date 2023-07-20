7ad0f0f54a1c38d86044cb4441e53d9adc1ddbae
onasu66 <139845323+onasu66@users.noreply.github.com>
2023-07-20 19:05:37 +0900

    Add files via upload

--- ai_talk.py
+++ ai_talk.py
@@ -2,78 +2,13 @@ from fastapi import FastAPI, Request
 import openai
 from linebot import WebhookParser, LineBotApi
 from linebot.models import TextSendMessage
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.chains import ConversationalRetrievalChain
 import PyPDF2
 
 # 定数の定義
 OPENAI_API_KEY = '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'
 LINE_CHANNEL_ACCESS_TOKEN = 'vtNEjP6IvEOZy/kGGKQ4trYobJ7cx2khewDnigkqXq9MsiqGeuk94AVQ4XckF12O/62oawSQaJqC+zrZ2DDEVOXI+Yo5LVxoSlm6XnsQD9UrQn30wDEgeJm6VuTTmWxrEAQRkdAsqetSNTeXzIjvuQdB04t89/1O/w1cDnyilFU='
 LINE_CHANNEL_SECRET = '3971a771a03a8d6a9f8ee09f38a4ce94'
-# GPT-3.5-turbo用のtokenizerをimport
-from transformers import GPT2Tokenizer, GPT2TokenizerFast
-# GPT-3.5-turbo用のOpenAIEmbeddingsをimport
-from langchain.embeddings import OpenAIEmbeddings
 
-# Step 1: Convert PDF to text
-def pdf_to_text(file_path):
-    with open(file_path, 'rb') as file:
-        reader = PyPDF2.PdfFileReader(file)
-        text = ''
-        for page_num in range(reader.numPages):
-            page = reader.getPage(page_num)
-            text += page.extractText()
-    return text
-
-# PDFファイルのパスを指定してテキストに変換un
-pdf_file_path = "./生活保護運用事例 集 2017（令和3年6月改訂版）.pdf"
-text = pdf_to_text(pdf_file_path)
-
-# Step 2: Save to .txt and reopen (helps prevent issues)
-with open('生活保護運用事例 集 2017（令和3年6月改訂版）.txt', 'w') as f:
-    f.write(text)
-
-with open('生活保護運用事例 集 2017（令和3年6月改訂版）.txt', 'r') as f:
-    text = f.read()
-
-# Step 3: Create function to count tokens
-tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
-
-def count_tokens(text: str) -> int:
-    return len(tokenizer.encode(text))
-
-# Step 4: Split text into chunks
-text_splitter = CharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size=512,
-    chunk_overlap=24,
-    length_function=count_tokens,
-)
-
-chunks = text_splitter.create_documents([text])
-# Result is many LangChain 'Documents' around 500 tokens or less (Recursive splitter sometimes allows more tokens to retain context)
-type(chunks[0]) 
-# Get embedding model
-embeddings = OpenAIEmbeddings("EleutherAI/gpt-neo-2.7B")
-
-# Create vector database
-db = FAISS.from_documents(chunks, embeddings)
-# Check similarity search is working
-query = "Who created transformers?"
-docs = db.similarity_search(query)
-docs[0]
-# Create QA chain to integrate similarity search with user queries (answer query from knowledge base)
-
-chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-
-query = "Who created transformers?"
-docs = db.similarity_search(query)
-
-chain.run(input_documents=docs, question=query)
 
 # ChatGPTのプロファイルを定義
 OPENAI_CHARACTER_PROFILE = '''
